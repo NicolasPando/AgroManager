f394911def35ade3d3a43ad0752e63122ecca289
NicolasPando <nicolas.g.pando@gmail.com>
2024-07-31 18:43:36 -0300

    test devito mensual

--- proyecto-pf/src/payment/payment.controller.ts
+++ proyecto-pf/src/payment/payment.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Param, ParseUUIDPipe, Query, Res } from "@nestjs/commo
 import { ApiTags } from "@nestjs/swagger";
 import { UUID } from "crypto";
 import { Response } from 'express';
-import mercadopago, { MercadoPagoConfig, Preference } from 'mercadopago';
+import mercadopago, { MercadoPagoConfig, PreApproval, Preference } from 'mercadopago';
 
 const client = new MercadoPagoConfig({ accessToken: 'APP_USR-8872127632320015-073117-44c82f425496d87fef92fc5bad1f0552-1490519917' });
 
@@ -12,23 +12,21 @@ export class PaymentController {
     @Get("monthly/:id")
     async suscripcionMensual(@Param("id", ParseUUIDPipe) id: UUID) {
         const body = {
-            items: [{
-                id: '1234', 
-                title: "suscripcion mensual agromanager",
-                quantity: 1,
-                unit_price: 1,
-                currency_id: "ARS"
-            }],
-            back_urls: {
-                success: `https://deppf.onrender.com/users/premium/monthly/${id}`,
-                failure: "https://music.youtube.com/watch?v=jtXDIfWjMkQ",
-                pending: "https://www.youtube.com/watch?v=vEXwN9-tKcs",
+            reason: "suscripcion mensual agromanager",
+            auto_recurring: {
+                frequency: 1,
+                frequency_type: "days",
+                transaction_amount: 1,
+                currency_id: "ARS",
+                start_date: new Date().toISOString(),
+                end_date: new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString()
             },
-            auto_return: "approved"
+            back_url: `https://tu-dominio-publico.com/users/premium/monthly/${id}`,
+            payer_email: "test_user_123456@testuser.com"
         };
 
-        const preference = new Preference(client);
-        const result = await preference.create({ body });
+        const preapproval = new PreApproval(client);
+        const result = await preapproval.create({ body });
         return result.init_point;
     }
 
